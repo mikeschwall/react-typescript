bb25514b3c9f18b852cbfaeb44c7311897070a41
Mike Schwall <mikschwall@gmail.com>
2025-03-21 14:22:26 -0500

    enhancements

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import { useState,FormEvent } from "react";
 import { getPackages, Package } from "../actions";
 import { AppState } from "../reducers";
-
+import ItemModal from "./ItemModal";
 
 
 interface StateProps {
@@ -21,6 +21,7 @@ const Search:React.FC<MergedProps> = ({packages,getPackages}) => {
 
     const [text,setText] = useState('');
     
+    
     const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
         setText(event.target.value);
     }
@@ -28,17 +29,20 @@ const Search:React.FC<MergedProps> = ({packages,getPackages}) => {
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         getPackages(text);
+        setText('');
     }
 
     return (
         <div style={{marginLeft:"20px"}}>
-            <h2>Search NPM Registry</h2>
+            <div className="page-header">
+  <h1>Search NPM Registry</h1>
+</div>
             <form onSubmit={handleSubmit}>
             <input placeholder="e.g jquery" onChange={handleChange} type="text" value={text} /> <button type="submit">Search</button>
             </form>
 
             {packages && packages.map((item) => <li key={item.package.name}>{item.package.name} 
-                <button style={{margin:"5px"}} onClick={() => alert(item.package.description)}>Info</button>
+            <ItemModal desc={item.package.description}/>
             </li>)}
         </div>
     )
